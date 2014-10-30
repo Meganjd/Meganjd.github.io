0eda91bd1d0b596c2a6f8f76c3bd1c76821ec175
Meganjd <meganjd@hotmail.com>
2014-10-30 15:30:24 -0400

    changed js for arrow

--- js/arrow.js
+++ js/arrow.js
@@ -1,12 +1,17 @@
-  var imgDescription = document.querySelector('.img-description');
+var downArrow = document.querySelectorAll('.down-arrow');
+var imgDesc = document.querySelectorAll('.img-desc');
 
-  document.querySelector('.down-arrow').addEventListener('click', function(e){
-  e.preventDefault();
-    
-  if(aboutDesc.getAttribute('data-state') == 'expanded'){
-	  aboutDesc.setAttribute('data-state', 'collapsed');  
-  }else{
-     	  aboutDesc.setAttribute('data-state', 'expanded');  
-  }
-  
-  });
+for (var i=0;i<downArrow.length;i++){
+    downArrow.item(i).addEventListener("click", myFunction);
+}
+
+
+function myFunction() {
+    var num = this.getAttribute('data-num');
+    if (imgDesc.item(num).getAttribute('data-state') == 'expanded') {
+        imgDesc.item(num).setAttribute('data-state', 'collapsed');
+    }else{
+        imgDesc.item(num).setAttribute('data-state', 'expanded');
+    }
+    
+}
